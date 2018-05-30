a6c7692010d4a4c31bae78cbf86c67ce43b129bd
admq <970255897@qq.com>
2018-05-30 23:44:08 +0800

    添加tabBar

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,7 +3,22 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
+    phoneNumber: '0731-88738273',
     logs: []
+  },
+  // 打电话
+  phoneCall: function(e) {
+    console.log(e)
+    wx.makePhoneCall({
+      phoneNumber: this.data.phoneNumber
+    })
+  },
+  // 地图
+  openMap: function(e) {
+    wx.openLocation({
+      latitude: 39.908856,
+      longitude: 116.397395
+    })
   },
   onLoad: function () {
     this.setData({
